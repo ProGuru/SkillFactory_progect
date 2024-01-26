3333c1641210a56ed7ee9f0d006a47f7bdc9c28b
Дмитрий Земсков <zemsckov.dmitry@yandex.ru>
2024-01-27 00:27:03 +0300

    Коммит 3

---
+++ new_code.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string hello = "Hello";
+
+        Print(hello);
+
+        void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
